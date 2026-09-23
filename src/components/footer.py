0173beb4e98823b10020aa117555f1ed12e75801
Nehan Pathan <pathannehan26@gmail.com>
2026-09-23 21:03:36 +0530

    feat: add SmartClass AI Attendance application

---
+++ src/components/footer.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+
+def footer_home():
+    logo_url = "https://nehan-portfolio.netlify.app/static/media/profile-img.80910be53b52d34fdb50.png"
+
+    st.markdown(
+        f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Nehan </p>
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+
+                """,
+        unsafe_allow_html=True,
+    )
+
+
+def footer_dashboard():
+    logo_url = "https://nehan-portfolio.netlify.app/static/media/profile-img.80910be53b52d34fdb50.png"
+
+    st.markdown(
+        f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by Nehan </p>
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+
+                """,
+        unsafe_allow_html=True,
+    )
